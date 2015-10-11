e5ce5b8bab994b1e1d752706287704f68ba07c6f
Connor Sears <connor.sears@github.com>
2015-10-10 19:26:38 -0700

    playing with context menus

--- example-app/js/index.js
+++ example-app/js/index.js
@@ -16,7 +16,16 @@ menu.append(new MenuItem({
   }
 }))
 
-document.addEventListener('contextmenu', function (event) {
-  event.preventDefault()
-  menu.popup(remote.getCurrentWindow())
-}, false)
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.js-context-menu').addEventListener('click', function (event) {
+    // var BrowserWindow = require('remote').require('browser-window')
+    //
+    // var button = event.target
+    // var windowPosition = BrowserWindow.getFocusedWindow().getPosition()
+    //
+    // var x = button.offsetLeft + windowPosition[0] + (button.offsetWidth / 2)
+    // var y = button.offsetTop + windowPosition[1] + button.offsetHeight
+
+    menu.popup(remote.getCurrentWindow());
+  })
+})
