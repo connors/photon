2f02df2b562507ad6b4c8e40a7d1c852bc044806
con-ssc <con-ssc@users.noreply.github.com>
2016-05-17 16:14:28 +0200

    Update and rename app.js to main.js
    Module name fixes
    BrowserWindow settings fixes

---
+++ dist/template-app/main.js
@@ -0,0 +1,54 @@
+const electron = require('electron');
+
+// Module to control application life.
+const {app} = electron;
+
+// Module to create native browser window.
+const {BrowserWindow} = electron;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow = null;
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 300
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
+});
