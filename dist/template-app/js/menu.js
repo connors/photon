4de64e8d15ee30c9e61164797c7d74f862238756
samuelyeshua <samoel_deltree@live.com>
2016-07-26 15:49:05 -0300

    Fix a problem in the context menu

--- dist/template-app/js/menu.js
+++ dist/template-app/js/menu.js
@@ -1,6 +1,8 @@
-var remote = require('remote')
-var Menu = remote.require('menu')
-var MenuItem = remote.require('menu-item')
+// var remote = require('remote')
+// var Menu = remote.require('menu')
+// var MenuItem = remote.require('menu-item')
+const {remote} = require('electron')
+const {Menu,MenuItem} = remote
 
 // Build our new menu
 var menu = new Menu()
@@ -21,7 +23,13 @@ menu.append(new MenuItem({
 
 // Add the listener
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.js-context-menu').addEventListener('click', function (event) {
-    menu.popup(remote.getCurrentWindow());
+
+  let filesContext = document.querySelectorAll('.file_arq');
+  
+  filesContext.forEach(function(el){
+    el.addEventListener('click', function (event) {
+      event.preventDefault()
+      menu.popup(remote.getCurrentWindow());
+    })
   })
 })
