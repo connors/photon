4e78dc2c94692f5625d02828c612b5d05f4e2276
ProPuke <ProPuke@31i73.com>
2017-06-02 19:39:46 +0100

    added: js lib and support for table/nav/list selections
    tables, list and things should now be automatically selectable if the js lib is included, and support multiple selection if `data-select-multiple` is set on the table/list
    Upon selection change the `change` event is triggered for the parent table/list

---
+++ dist/js/photon.js
@@ -0,0 +1,51 @@
+const $ = require('jquery/dist/jquery.js');
+
+{
+	let toggling = false;
+	let togglingState = false;
+
+	$(document)
+		.on('mousedown', 'table[data-select-multiple] > tbody > tr, .nav-group[data-select-multiple] > .nav-group-item, .list-group[data-select-multiple] > .list-group-item', function(event){
+			const $this = $(this);
+
+			if((event.ctrlKey||event.metaKey)){
+				toggling = true;
+				togglingState = !$this.hasClass('active');
+				$this.toggleClass('active');
+				$(document).one('mouseup', () => {
+					toggling = false;
+				});
+
+			}else{
+				$this.addClass('active').siblings().removeClass('active');
+			}
+
+			$this.closest('[data-select-multiple]').trigger('change');
+		})
+		.on('mouseenter', 'table[data-select-multiple] > tbody > tr, .nav-group[data-select-multiple] > .nav-group-item, .list-group[data-select-multiple] > .list-group-item', function(event){
+			const $this = $(this);
+
+			if(toggling){
+				$this.toggleClass('active', togglingState);
+
+			}else if(event.buttons&1){
+				$this.addClass('active').siblings().removeClass('active');
+
+			}else{
+				return;
+			}
+
+			$this.closest('[data-select-multiple]').trigger('change');
+		})
+		.on('mousedown', 'table:not([data-select-multiple]) > tbody > tr', function(event){
+			const $this = $(this);
+			$this.addClass('active').siblings().removeClass('active');
+			$this.parent().parent().trigger('change');
+		})
+		.on('mousedown', '.nav-group:not([data-select-multiple]) > .nav-group-item, .list-group:not([data-select-multiple]) > .list-group-item', function(event){
+			const $this = $(this);
+			$this.addClass('active').siblings().removeClass('active');
+			$this.parent().trigger('change');
+		})
+	;
+}
